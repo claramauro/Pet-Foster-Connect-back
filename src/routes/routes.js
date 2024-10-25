3b0c5bfa69c9dd0133752c9d8e7e7e3adc251db1
Sloan Gauthier <sloangauthier@gmail.com>
2024-10-25 22:06:17 +0200

    Update the dashboard route name to specify that it is the association dashboard routes + add --legacy-watch on nodemon script to patch no auto restart server

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { associationsRoutes } from "./associations.routes.js";
 import { animalsRoutes } from "./animals.routes.js";
-import { dashboardRoutes } from "./dashboard.routes.js";
+import { dashboardAssociationRoutes } from "./dashboardAssociation.routes.js";
 
 
 const router = Router();
 
 router.use("/associations", associationsRoutes);
 router.use("/animals", animalsRoutes);
-router.use("/dashboard", dashboardRoutes);
+router.use("/dashboard", dashboardAssociationRoutes);
 
 router.get("/", (req, res) => {
     /*
