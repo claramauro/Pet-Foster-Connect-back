3769595334b005a54b44ee5afbcfd5ae7b338033
Sloan Gauthier <sloangauthier@gmail.com>
2024-10-25 10:33:55 +0200

    add package prettier

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ dotenv.config();
 const app = express();
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
