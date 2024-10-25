f981963ecd8ce306518aba0acdd78171963ed4eb
Sloan Gauthier <sloangauthier@gmail.com>
2024-10-25 22:55:24 +0200

    Initializing Auth Routes and Controllers

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { authController } from "../controllers/auth.controller.js";
+
+
+const authRoutes = Router();
+
+authRoutes.post("/register", authController.register);
+authRoutes.post("/login", authController.login);
+authRoutes.post("/logout", authController.logout);
+
+export { authRoutes };
