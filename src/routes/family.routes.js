a5db1aedae335240ff16363059185410ba151b9e
Sloan Gauthier <sloangauthier@gmail.com>
2024-10-28 19:01:36 +0100

    Wrap controllers on catchErrors

--- src/routes/family.routes.js
+++ src/routes/family.routes.js
@@ -5,7 +5,7 @@ import { catchErrors } from "../middlewares/catchError.js";
 const familyRoutes = Router();
 
 familyRoutes.get("/:id(\\d+)", catchErrors(familyController.findOne));
-familyRoutes.patch("/:id(\\d+)", familyController.update);
-familyRoutes.delete("/:id(\\d+)", familyController.destroy);
+familyRoutes.patch("/:id(\\d+)", catchErrors(familyController.update));
+familyRoutes.delete("/:id(\\d+)", catchErrors(familyController.destroy));
 
 export { familyRoutes };
