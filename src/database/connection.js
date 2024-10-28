1ffeeba162db27fce3acd60e1d7d7e1592242efb
camille <camille.nerriere@yahoo.fr>
2024-10-28 09:34:43 +0100

    associations updated

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,18 +3,13 @@ dotenv.config();
 
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-	process.env.POSTGRES_DB,
-	process.env.POSTGRES_USER,
-	process.env.POSTGRES_PASSWORD,
-	{
-		dialect: "postgres",
-		define: {
-			createdAt: "created_at",
-			updatedAt: "updated_at",
-			underscored: true,
-		},
-	},
-);
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+    dialect: "postgres",
+    define: {
+        createdAt: "created_at",
+        updatedAt: "updated_at",
+        underscored: true,
+    },
+});
 
 export { sequelize };
